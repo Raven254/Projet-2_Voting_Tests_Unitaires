a4f4b8dccaeff86793f7ed62fd232f9d919707f8
0xMeurwo <marwaane.eljaafari@gmail.com>
2022-06-28 16:58:11 +0200

    Ajout du test des états.

--- test/testingVoting.test.js
+++ test/testingVoting.test.js
@@ -13,21 +13,128 @@ contract("Voting", accounts => {
 
     let VotingInstance;
 
-    describe("Test de type X", function() {
-        beforeEach(async() => {
-            VotingInstance = await Voting.new({from: owner});
+    describe("Test de workflowStatus", function() {
+        context("Test du fonctionnement des require pour le changement d'état, à l'état 0.", function() {
+            before(async() => {
+                VotingInstance = await Voting.new({from: owner});
+            });
+
+            it("Il est bien impossible de passer à ProposalsRegistrationEnded à l'état 0", async() => {
+                await expectRevert(VotingInstance.endProposalsRegistering({from: owner}), 'Registering proposals havent started yet');
+            });
+
+            it("Il est bien impossible de passer à VotingSessionStarted à l'état 0", async() => {
+                await expectRevert(VotingInstance.startVotingSession({from: owner}), 'Registering proposals phase is not finished');
+            });
+
+            it("Il est bien impossible de passer à VotingSessionEnded à l'état 0", async() => {
+                await expectRevert(VotingInstance.endVotingSession({from: owner}), 'Voting session havent started yet');
+            });
+
+            it("Il est bien impossible de passer à VotesTallied à l'état 0", async() => {
+                await expectRevert(VotingInstance.tallyVotes({from: owner}), 'Current status is not voting session ended');
+            });
         });
+        
+        context("Test du déroulé de workflowStatus.", function() {
+            before(async() => {
+                VotingInstance = await Voting.new({from: owner});
+            });
 
-        it("Cela devrait faire action Y", async() => {
+            it("L'état par défault est bien Registering Voters", async() => {
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(0));
+            });
 
-        });
+            it("Le passage à ProposalsRegistrationStarted fonctionne", async() => {
+                await VotingInstance.startProposalsRegistering({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(1));
+            });
 
-        it("Cela devrait faire action Z", async() => {
+            it("Le passage à ProposalsRegistrationEnded fonctionne", async() => {
+                await VotingInstance.endProposalsRegistering({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(2));
+            });
 
-        });
+            it("Le passage à VotingSessionStarted fonctionne", async() => {
+                await VotingInstance.startVotingSession({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(3));
+            });
 
-        it("Cela devrait faire action W", async() => {
+            it("Le passage à VotingSessionEnded fonctionne", async() => {
+                await VotingInstance.endVotingSession({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(4));
+            });
 
-        });
-    })
+            it("Le passage à VotesTallied fonctionne", async() => {
+                await VotingInstance.tallyVotes({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(5));
+            });
+
+        
+        context("Test des événements pour workflowStatus.", function() {
+            before(async() => {
+                VotingInstance = await Voting.new({from: owner});
+            });
+
+            it("L'état par défault est bien Registering Voters", async() => {
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(0));
+            });
+
+            it("Le passage à ProposalsRegistrationStarted fonctionne", async() => {
+                await VotingInstance.startProposalsRegistering({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(1));
+            });
+
+            it("Le passage à ProposalsRegistrationEnded fonctionne", async() => {
+                await VotingInstance.endProposalsRegistering({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(2));
+            });
+
+            it("Le passage à VotingSessionStarted fonctionne", async() => {
+                await VotingInstance.startVotingSession({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(3));
+            });
+
+            it("Le passage à VotingSessionEnded fonctionne", async() => {
+                await VotingInstance.endVotingSession({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(4));
+            });
+
+            it("Le passage à VotesTallied fonctionne", async() => {
+                await VotingInstance.tallyVotes({from: owner});
+                const stateData = await VotingInstance.workflowStatus.call();
+                expect(new BN(stateData)).to.be.bignumber.equal(new BN(5));
+            });
+    });
+
+    //describe("Test du Whitelisting", function() {
+    //    beforeEach(async() => {
+    //        VotingInstance = await Voting.new({from: owner});
+    //    });
+
+    //    it("Cela devrait faire action Y", async() => {
+
+    //    });
+
+    //    it("Cela devrait faire action Z", async() => {
+
+    //    });
+
+    //    it("Cela devrait faire action W", async() => {
+
+    //    });
+    //})
+});
+});
 })
